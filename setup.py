b5c4c34cce803ec1d545aac006210b1e9677d9a4
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2017-12-11 08:46:14 -0500

    add pip, travis, and pytest
    change module path to bsdetector to remove import underscore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+package = 'bsdetector'
+version = '0.1'
+
+print(find_packages('bsdetector'))
+setup(name=package,
+      version=version,
+      packages=['bsdetector','ref_lexicons', 'additional_resources'],
+      install_requires=['vaderSentiment'], 
+      description="Detects biased statements in online media documents",
+      url='url')
---
+++ test/test_main.py
@@ -0,0 +1,6 @@
+import pytest
+
+def test_imports():
+    import bsdetector
+    import vaderSentiment
+    return True
